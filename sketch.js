d45d6686f791d76358c9eeab9048d13b475c424d
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-05-13 08:00:39 +0530

    Small movement changes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+var y;
 function setup() {
   createCanvas(windowWidth/2, windowHeight);
+  y = 0;
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  y = y +1;
+  for (let i = 0; i < windowHeight; i++) {
+    noFill();
+    stroke(255);
+    // frameRate(60);
+    if (y < 100){
+      bezier(10, i*25, 200, i*25+y, 300, i*25, windowWidth/2-10, i*25);
+    }
+    else{
+      bezier(10, i*25, 200, i*25-y, 300, i*25, windowWidth/2-10, i*25);
+      // y = y-1;
+    }
+    
+  }
 }
