290b7f2a5a777efe6bc7d6dc5aa8fff2496bac05
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-05-13 12:53:56 +0530

    Reactions added

--- sketch.js
+++ sketch.js
@@ -1,23 +1,97 @@
 var y;
+var calmSelection = true;
+var disturbedSelection = false;
+var sound;
+var amp;
+
+function preload(){
+  sound = loadSound("Glitch.mp3")
+}
+
 function setup() {
-  createCanvas(windowWidth/2, windowHeight);
+  let removal = 0;
+  createCanvas(windowWidth/2, 500);
+  sound.play();
+  // sound.setVolume(0.5);
+  amp = new p5.Amplitude();
   y = 0;
 }
 
 function draw() {
   background(0);
+  frameRate(30);
+  var waveHeight = amp.getLevel();
 
+  if (waveHeight < 0.3){
+    calmMind()
+  }
+  else{
+    disturbedMind()  
+  }
+  // if (calmSelection){
+  //   calmMind()
+  // }
+  // if (disturbedSelection){
+  //   disturbedMind()  
+  // }
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    calmSelection = false;
+    disturbedSelection = true;
+  } 
+  else if (keyCode === RIGHT_ARROW) {
+    calmSelection = true;
+    disturbedSelection = false;
+  }
+}
+
+function calmMind(){
   y = y +1;
   for (let i = 0; i < windowHeight; i++) {
-    noFill();
-    stroke(255);
-    // frameRate(60);
     if (y < 500){
-      bezier(10, i*25, 200, random(i*25+y, 1), 300, i*25, windowWidth/2-10, i*25);
+      noFill();
+      strokeWeight(2);
+      smooth();
+      if (i%2 == 0){
+        stroke(220);
+        // bezier(10, i*25, random(200), i*25+y, random(500,650), i*25-y, windowWidth/2-10, i*25);
+        bezier(10, i*25, 200, i*25+y, 300, i*25-y, windowWidth/2-10, i*25);
+      }
+      else{
+        stroke(255);
+        bezier(10, i*25, 200, i*25+y, 300, i*25-y, windowWidth/2-10, i*25);
+      }
     }
     else{
-      y = -10;
+      y = -500;
+    } 
+  } 
+}
+
+function disturbedMind(){
+  y = y +1;
+  for (let i = 0; i < windowHeight; i++) {
+    if (y < 500){
+      noFill();
+      strokeWeight(2);
+      smooth();
+      if (i%2 == 0){
+        stroke(220);
+        bezier(10, random(i*25), 200, i*25+y, 300, i*25-y, windowWidth/2-10, random(i*25));
+        // bezier(10, random(i*25), random(200), i*25+y, random(500,650), i*25-y, windowWidth/2-10, random(i*25));
+        // bezier(10, i*25, 200, i*25+y, 300, i*25-y, windowWidth/2-10, i*25);
+      }
+      else{
+        stroke(255);
+        bezier(10, random(i*25), 200, i*25+y, 300, i*25-y, windowWidth/2-10, random(i*25));
+        // bezier(10, i*25, random(200), i*25+y, random(500,650), i*25-y, windowWidth/2-10, i*25);
+        // bezier(10, i*25, 200, i*25+y, 300, i*25-y, windowWidth/2-10, i*25);
+      }
     }
-    
-  }
+    else{
+      y = -500;
+    } 
+  } 
 }
